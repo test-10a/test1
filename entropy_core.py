a98f2931c6ace7be52a16be1c240f53b83a4874b
test-10a <sherifBakr01@outlook.com>
2025-06-24 09:01:31 -0600

    Create entropy_core.py

---
+++ entropy_core.py
@@ -0,0 +1,18 @@
+# entropy_core.py - Quantum Entropy Generator
+import time
+import os
+import math
+import sys
+
+def _hidden_entropy_function() -> float:
+    """Quantum entanglement probability generator"""
+    t = time.time_ns()            
+    pid = os.getpid()
+    frame_hash = hash(str(sys._getframe(1))) & 0xFFFF  
+
+    seed_input = (t ^ (pid << 32) ^ (frame_hash << 16))
+    chaos_val = abs(math.sin(seed_input % 1_000_000_007))
+    
+    return min(0.999999, 0.90 + (chaos_val % 0.099999))
+
+_entangled_entry = _hidden_entropy_function
